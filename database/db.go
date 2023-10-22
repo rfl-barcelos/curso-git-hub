a3f8ec3d73836cb391b39550ea625cb7f2e60d15
Rafael Barcelos <rfl.barcelos1@gmail.com>
2023-10-21 22:01:35 -0300

    ajustando arquivo de conf do banco para trabalhar com variaveis

--- database/db.go
+++ database/db.go
@@ -13,8 +13,15 @@ var (
 	err error
 )
 
+
+
 func ConectaComBancoDeDados() {
-	stringDeConexao := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
+    	host := os.Getenv("HOST")
+	user := os.Getenv("USER")
+    	password := os.Getenv("PASSWORD")
+    	dbname := os.Getenv("DBNAME")
+    	port := os.Getenv("DBPORT")
+    	stringDeConexao := "host="+ host +" user=" + user + " password=" + password + " dbname=" + dbname + " port=" + port + " sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
